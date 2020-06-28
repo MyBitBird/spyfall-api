9065207e33f9f7496eddec7b490097634608666a
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-06-28 16:37:29 +0430

    using JSON

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const rooms = require('./rooms');
 const games = require('./games');
+const express = require('express');
 
 const routes = (app) =>
 {
-    app.use('/api/rooms' , rooms);
+    app.use(express.json());
+    app.use('/api/rooms/' , rooms);
     app.use('/api/games' , games);
 }
 
--- src/routes/rooms.js
+++ src/routes/rooms.js
@@ -1,10 +1,34 @@
-const express = require('express');
+const _ = require("lodash");
+const express = require("express");
+const router = express.Router();
 
-const route = express.Router();
+const { isValid } = require("../models/room");
+const service = require("../services/room");
 
-const get = route.get('/' , (req , res) =>{
-    res.status(200).send('Rooms');
+router.post("/", async (req, res) => {
+  const validation = isValid(req.body);
+  if (validation.error)
+    return res.status(400).send(validation.error.details[0].message);
 
+  const result = await service.create(req.body);
+  res.status(200).send(mapToDto(result));
 });
 
-module.exports = route;
+router.patch("/:code", async (req, res) => {
+  const validation = isValid(req.body);
+  if (validation.error)
+    return res.status(400).send(validation.error.details[0].message);
+
+  const room = await service.findByCode(req.params.code);
+  if (!room) return res.status(404).send("Room Not Found");
+
+  const result = await service.join(room, req.body);
+  res.status(200).send(mapToDto(result));
+});
+
+const mapToDto = result => {
+  const dto = _.pick(result, ["_id", "code"]);
+  return { ...dto, playerId: result.players[result.players.length - 1]._id };
+};
+
+module.exports = router;
