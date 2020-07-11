3054c70d21762aa2d009e47bbe42fc9da043cf24
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-07-11 20:36:32 +0430

    cleaning code -primary websocket functions

--- src/websocket/index.js
+++ src/websocket/index.js
@@ -1,11 +1,14 @@
 const http = require("http");
 const socketClients = [];
 
-module.exports = (app, PORT) => {
+const connect = (app, PORT, callback) => {
   const server = http.createServer(app);
   const socket = require("socket.io")(server);
-  socket.on("connected", (socket) => {
-    onConnected(socket);
+  socket.on("connection", (socket) => {
+    onConnected(socket, callback);
+    socket.on("disconnect", () => {
+      onDisconnected(socket);
+    });
   });
 
   server.listen(PORT, () => {
@@ -13,11 +16,34 @@ module.exports = (app, PORT) => {
   });
 };
 
-const onConnected = (socket) => {
-  const roomId = socket.handshake.query.id;
-  console.log(" a user connected " + roomId, socket);
-  const isExist = socketClients.findIndex((x) => x.roomId === roomId);
-  const socketClient = { id: socket.id, roomId: roomId, socket: socket };
-  if (isExist >= 0) socketClients.splice(isExist, 1, socketClient);
-  else socketClients.push(socketClient);
+const onConnected = async (socket, callback) => {
+  const joinId = socket.handshake.query.id;
+  console.log(" a user connected ", socket.id);
+
+  const isExist = socketClients.findIndex((x) => x.id === socket.id);
+  if (isExist >= 0) return; //socketClients.splice(isExist, 1, socketClient);
+  
+  const socketClient = { id: socket.id, roomId: joinId, socket: socket };
+  socketClients.push(socketClient);
+  callback(joinId);
 };
+
+const onDisconnected = (socket) => {
+  console.log(" a user disconnected ", socket.id);
+  socket.disconnect();
+  const itemIndex = socketClients.findIndex((x) => x.id === socket.id);
+  socketClients.splice(itemIndex, 1);
+};
+
+const send = (joinId, event, data) => {
+  const clients = socketClients.filter((x) => x.roomId === joinId);
+  if (!clients.length) return false;
+
+  clients.forEach((client) => {
+    console.log("send to ", clients.length);
+    client.socket.emit(event + joinId, data);
+  });
+  return true;
+};
+
+module.exports = { connect, send };
