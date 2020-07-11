cce3c31ae4fe9a5d3b124c8fa50dd94be3c19f6a
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-07-11 20:38:53 +0430

    using cord midleware

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 const rooms = require('./rooms');
 const games = require('./games');
 const express = require('express');
+const cordPolicy = require('../midleware/cors')
 
 const routes = (app) =>
 {
     app.use(express.json());
+    app.use(cordPolicy);
     app.use('/api/rooms/' , rooms);
-    app.use('/api/games' , games);
+    app.use('/api/games/' , games);
 }
 
 module.exports = routes;
