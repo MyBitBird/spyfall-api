d7b61a517ec4b8c793f6e3ad92ef2762280c9c16
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-07-11 20:43:36 +0430

    adding on socket connect callback

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const roomService = require('./services/room')
-require('./routes')(app);
-require('./db/db')();
+const roomService = require("./services/room");
+const config = require("config");
+const { connect: socketConnect, send } = require("./websocket/");
+require("./routes")(app);
+require("./db/db")();
 
 const PORT = process.env.PORT || 5500;
-app.listen(PORT , () =>{
-    console.log(`${PORT} connected...`)
-})
+const SOCKET_PORT = process.env.socketPort || 2000;
+
+socketConnect(app, SOCKET_PORT, async (roomId) => {
+  send(roomId, config.events.playerJoined, (await roomService.findById(roomId)).players);
+});
+
+app.listen(PORT, () => {
+  console.log(`${PORT} connected...`);
+});
