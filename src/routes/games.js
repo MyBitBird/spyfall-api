ee1d5d8ee495eaff2ded68a715320b190f678a39
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-07-12 17:11:33 +0430

    start and geting game added

--- src/routes/games.js
+++ src/routes/games.js
@@ -1,14 +1,22 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const auth = require('../midleware/auth')
+const auth = require("../midleware/auth");
+const service = require("../services/game");
+const roomService = require("../services/room");
+const events = require("../websocket/events");
 
-router.post('/' ,auth, (req , res)=>{
+router.post("/", auth, async (req, res) => {
+  const room = await roomService.findById(req.roomId);
+  const game = await service.startGame(room);
 
-})
+  events.onGameStarted(room._id, game._id);
+  res.status(200).send();
+});
 
-router.get('/' , auth , (req , res) =>{
-    res.status(200).send('games');
-    
-})
+router.get("/:id", auth, async (req, res) => {
+  const game = await service.findById(req.params.id);
+  if (req.playerId == game.spyId) return res.status(200).send("You are Spy!");
+  return res.status(200).send(game.location);
+});
 
 module.exports = router;
