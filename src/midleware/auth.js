3fac3b4f43900ef8c72fc2367d18b583d411d450
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-07-12 13:22:10 +0430

    auth midleware

---
+++ src/midleware/auth.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+module.exports = (req, res, next) => {
+  const token = req.headers["authorization"];
+  if (!token) return res.status(401).send("Access Denied!");
+
+  try {
+    const payload = jwt.verify(token, config.tokenSecretKey);
+    console.log('payload' , payload)
+    req.playerId = payload._id;
+    req.roomId = payload.roomId;
+    next();
+  } catch (ex) {
+    res.status(400).send("Invalid Token.");
+  }
+};
--- src/midleware/cors.js
+++ src/midleware/cors.js
@@ -1,8 +1,9 @@
 const cors = (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Request-Headers");
     res.header(
       "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
+      "Origin, X-Requested-With, Content-Type, Accept,Authorization"
     );
     res.header(
       "Access-Control-Allow-Methods",
