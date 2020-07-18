0ae6d40c51ce1883609726cc3657be47d0398b63
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-07-18 19:02:45 +0430

    using one port for http server and socket

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const roomService = require("./src/services/room");
 const config = require("config");
-const { connect: socketConnect, send } = require("./src/websocket");
+const { connect, send } = require("./src/websocket");
 const events = require('./src/websocket/events')
 const helmet = require('helmet');
 if(!config.tokenSecretKey)
@@ -16,10 +16,8 @@ require("./src/routes")(app);
 require("./src/db/db")();
 
 const PORT = process.env.PORT || 5500;
-const SOCKET_PORT = process.env.socketPort || 2000;
+connect(app, PORT, events.onRoomPlayersChanged);
 
-socketConnect(app, SOCKET_PORT, events.onRoomPlayersChanged);
-
-app.listen(PORT, () => {
-  console.log(`${PORT} connected...`);
-});
+//app.listen(PORT, () => {
+//  console.log(`${PORT} connected...`);
+//});
