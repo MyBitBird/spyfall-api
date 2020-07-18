b90149c6a0b37c0f1416c792aa77069cf59ee03e
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-07-18 17:58:32 +0430

    moving app and config to right place

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const app = express();
-const roomService = require("./services/room");
+const roomService = require("./src/services/room");
 const config = require("config");
-const { connect: socketConnect, send } = require("./websocket/");
-const events = require('./websocket/events')
-
+const { connect: socketConnect, send } = require("./src/websocket");
+const events = require('./src/websocket/events')
+const helmet = require('helmet');
 if(!config.tokenSecretKey)
 {
   console.log('ERROR: jwtPrivateToken is not defined.')
   process.exit(1);
 }
 
-require("./routes")(app);
-require("./db/db")();
+app.use(helmet())
+require("./src/routes")(app);
+require("./src/db/db")();
 
 const PORT = process.env.PORT || 5500;
 const SOCKET_PORT = process.env.socketPort || 2000;
